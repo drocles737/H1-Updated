8a4a44887fc9c95093cf293e777c082326d8738b
drocles737 <37443835+drocles737@users.noreply.github.com>
2019-02-18 08:36:47 +0100

    Add files via upload

---
+++ H1opgave auto/H1opgave auto/Biler.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace H1opgave_auto
+{
+    class Biler
+    {
+        private string Regnr;
+        private string Mærke;
+        private string Model;
+        private int Årgang;
+        private int Km;
+        private string Brændstoftype;
+
+        public string myregnr
+        {
+            get { return Regnr; }
+            set { Regnr = value; }
+        }
+
+        public string mymærke
+        {
+            get { return Mærke; }
+            set { Mærke = value; }
+        }
+
+        public string mymodel
+        {
+            get { return Model; }
+            set { Model = value; }
+        }
+
+        public int myårgang
+        {
+            get { return Årgang; }
+            set { Årgang = value; }
+        }
+
+        public int mykm
+        {
+            get { return Km; }
+            set { Km = value; }
+        }
+
+        public string mybrændstof
+        {
+            get { return Brændstoftype; }
+            set { Brændstoftype = value; }
+        }
+
+        public Biler()
+        {
+
+        }
+        
+        public Biler(string mitregnr, string mitmærke, string mitmodel, int mitårgang, int mitkm, string mitbrændstof)
+        {
+            Regnr = mitregnr;
+            Mærke = mitmærke;
+            Model = mitmodel;
+            Årgang = mitårgang;
+            Km = mitkm;
+            Brændstoftype = mitbrændstof;
+        }
+
+
+
+    }
+}
---
+++ H1opgave auto/H1opgave auto/Kunde.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace H1opgave_auto
+{
+    class Kunde
+    {
+        private int KundeID;
+        private string Fnavn;
+        private string Enavn;
+        private string Email;
+        private string Adresse;
+        private int Postnr;
+        private string By;
+        private int Tlfnr;
+
+        public int mykundeid
+        {
+            get { return KundeID; }
+            set { KundeID = value; }
+        }
+
+        public string myfnavn
+        {
+            get { return Fnavn;}
+            set { Fnavn = value; }
+        }
+
+        public string myenavn
+        {
+            get { return Enavn; }
+            set { Enavn = value; }
+        }
+
+        public string myemail
+        {
+            get { return Email; }
+            set { Email = value; }
+        }
+
+        public string myadress
+        {
+            get { return Adresse; }
+            set { Adresse = value; }
+        }
+
+        public int mypostnr
+        {
+            get { return Postnr; }
+            set { Postnr = value; }
+        }
+
+        public string myby
+        {
+            get { return By; }
+            set { By = value; }
+        }
+
+        public int mytlfnr
+        {
+            get { return Tlfnr; }
+            set { Tlfnr = value; }
+        }
+
+        public Kunde()
+        {
+
+        }
+
+        public Kunde(int kundeid, string fnavn, string enavn, string email, string adress, int postnr, string by, int tlfnr)
+        {
+            KundeID = kundeid;
+            Fnavn = fnavn;
+            Enavn = enavn;
+            Email = email;
+            Adresse = adress;
+            Postnr = postnr;
+            By = by;
+            Tlfnr = tlfnr;
+        }
+
+        
+    }
+}
